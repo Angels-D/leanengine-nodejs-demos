08e8878ab38ca2c1813cfcf1fd6f273dd0540c1d
とても残念だ <57611478+Angels-D@users.noreply.github.com>
2022-02-17 15:13:36 +0800

    Update websocket.js

--- routes/websocket.js
+++ routes/websocket.js
@@ -17,13 +17,19 @@ const router = module.exports = new Router
  *
  */
 
+const wss = ws(router).getWss('/')
+
 /*
  * 将客户端发来的消息原样发回客户端
  * wscat -c ws://localhost:3000/websocket/echo
 */
 router.ws('/echo', (ws, req) => {
   ws.on('message', (msg) => {
-    ws.send(msg)
+    if (msg ！= 'ping'){
+      wss.clients.forEach((e) => {
+        e.send(msg)
+      })
+    }
   })
 })
 
