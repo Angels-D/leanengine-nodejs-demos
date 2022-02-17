a3d4466e986cc3119eeef2b922fe33159fd2ad21
とても残念だ <57611478+Angels-D@users.noreply.github.com>
2022-02-17 16:03:34 +0800

    Update websocket.js

--- routes/websocket.js
+++ routes/websocket.js
@@ -26,7 +26,7 @@ const wss = require("express-ws")(router).getWss('/echo')
 router.ws('/echo', (ws, req) => {
   ws.on('message', (msg) => {
     if (msg != 'ping'){
-      expressWs(router).getWss('/echo').clients.forEach((e) => {e.send(msg)})
+      router.getWss().clients.forEach((e) => {e.send(msg)})
     }
   })
 })
