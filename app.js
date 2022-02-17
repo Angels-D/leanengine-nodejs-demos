6339f76ac9b2fd18f7ef0c59e76593e5fa00fdbd
とても残念だ <57611478+Angels-D@users.noreply.github.com>
2022-02-17 16:12:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express()
 // 启用 WebSocket 支持，如不需要可去除
 const expressWs = require('express-ws')
 expressWs(app)
+const wss = expressWs(app).getWss('/websocket/echo')
 
 // 设置模板路径和默认引擎
 app.set('views', path.join(__dirname, 'views'))
